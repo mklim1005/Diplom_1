57dc58307dadfc8121a8354b199ac95507c05138
Marina Klimova <m.klim1005@gmail.com>
2022-06-04 21:29:40 +0300

    Refactor TestBurger

--- src/test/java/praktikum/TestBurger.java
+++ src/test/java/praktikum/TestBurger.java
@@ -2,39 +2,110 @@ package praktikum;
 
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.junit.Assert.*;
 
 public class TestBurger {
 
     @Test
-    public void testBurgerAddIngredient(){
+    public void testBurgerAddIngredient() {
         Burger burger = new Burger();
-        Ingredient meat = new Ingredient(IngredientType.FILLING,"meat",70);
+        Ingredient meat = new Ingredient(IngredientType.FILLING, "my test meat", 70);
+
         burger.addIngredient(meat);
-        assertTrue(burger.ingredients.get(0) == meat);
+
+        assertEquals(1, burger.ingredients.size());
+        assertEquals(burger.ingredients.get(0).getType(), IngredientType.FILLING);
+        assertEquals(burger.ingredients.get(0).getName(), "my test meat");
+        assertEquals(burger.ingredients.get(0).getPrice(), 70, 0.0f);
     }
 
     @Test
-    public void testBurgerRemoveIngredient(){
+    public void testBurgerRemoveIngredient() {
         Burger burger = new Burger();
-        Ingredient meat = new Ingredient(IngredientType.FILLING,"meat",70);
+        Ingredient meat = new Ingredient(IngredientType.FILLING, "meat", 70);
         burger.addIngredient(meat);
+
         burger.removeIngredient(0);
+
         assertTrue(burger.ingredients.isEmpty());
     }
+
     @Test
-    public void testBurgerMoveIngredient(){
+    public void testBurgerMoveIngredient() {
         Burger burger = new Burger();
-        Ingredient meat = new Ingredient(IngredientType.FILLING,"meat",70);
-        Ingredient salad = new Ingredient(IngredientType.FILLING,"salad",20);
+        Ingredient meat = new Ingredient(IngredientType.FILLING, "meat", 70);
+        Ingredient salad = new Ingredient(IngredientType.FILLING, "salad", 20);
         burger.addIngredient(meat);
         burger.addIngredient(salad);
-        burger.moveIngredient(1,0);
-        assertTrue(burger.ingredients.get(0)==salad && burger.ingredients.get(1)==meat);
+
+        burger.moveIngredient(1, 0);
+
+        assertEquals(2, burger.ingredients.size());
+        assertEquals(burger.ingredients.get(0), salad);
+        assertEquals(burger.ingredients.get(1), meat);
     }
 
+    @Test
+    public void testPriceWithoutIngredients() {
+        Burger burger = new Burger();
+        Bun bun = new Bun("Super Bun", 1000);
+        burger.setBuns(bun);
 
+        float price = burger.getPrice();
 
+        assertEquals(2000, price, 0.0f);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testBurgerGetPriceWithoutBun_ShouldThrowException() {
+        Burger burger = new Burger();
+        Ingredient meat = new Ingredient(IngredientType.FILLING, "meat", 50);
+        Ingredient salad = new Ingredient(IngredientType.FILLING, "salad", 20);
+        Ingredient cheeseSauce = new Ingredient(IngredientType.SAUCE, "cheeseSauce", 10);
+        burger.addIngredient(meat);
+        burger.addIngredient(salad);
+        burger.addIngredient(cheeseSauce);
+
+        burger.getPrice();
+    }
+
+    @Test
+    public void testBurgerGetPrice() {
+        Burger burger = new Burger();
+        Bun bun = new Bun("Super Bun", 1000);
+        burger.setBuns(bun);
+        Ingredient meat = new Ingredient(IngredientType.FILLING, "meat", 50);
+        Ingredient salad = new Ingredient(IngredientType.FILLING, "salad", -20);
+        Ingredient cheeseSauce = new Ingredient(IngredientType.SAUCE, "cheeseSauce", 10);
+        burger.addIngredient(meat);
+        burger.addIngredient(salad);
+        burger.addIngredient(cheeseSauce);
+
+        float price = burger.getPrice();
+
+        assertEquals(2040, price, 0.0f);
+    }
+
+    @Test
+    public void testBurgerGetReceipt() {
+        Burger burger = new Burger();
+        Bun bun = new Bun("Super Bun", 40);
+        burger.setBuns(bun);
+        Ingredient meat = new Ingredient(IngredientType.FILLING, "meat", 70);
+        Ingredient salad = new Ingredient(IngredientType.FILLING, "salad", 20);
+        Ingredient cheeseSauce = new Ingredient(IngredientType.SAUCE, "cheeseSauce", 10);
+        burger.addIngredient(meat);
+        burger.addIngredient(salad);
+        burger.addIngredient(cheeseSauce);
+
+        String receipt = burger.getReceipt();
+
+        assertEquals("(==== Super Bun ====)\n" +
+                "= filling meat =\n" +
+                "= filling salad =\n" +
+                "= sauce cheeseSauce =\n" +
+                "(==== Super Bun ====)\n" +
+                "\n" +
+                "Price: 180.000000\n", receipt);
+    }
 }
