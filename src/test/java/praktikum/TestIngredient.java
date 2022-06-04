5e11a2c960b4080d728189e8b3c25827cbafd8d6
Marina Klimova <m.klim1005@gmail.com>
2022-06-04 21:40:53 +0300

    Refactor TestIngredient

--- src/test/java/praktikum/TestIngredient.java
+++ src/test/java/praktikum/TestIngredient.java
@@ -7,20 +7,26 @@ import static org.junit.Assert.assertEquals;
 public class TestIngredient {
 
     @Test
-    public void testIngredientGetPrice(){
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE,"Space Sauce",60);
-        assertEquals(60,ingredient.getPrice(),0.0f);
+    public void testIngredientGetPrice() {
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Space Sauce", 60.02f);
+        assertEquals(60.02f, ingredient.getPrice(), 0.0f);
     }
 
     @Test
-    public void testIngredientGetName(){
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE,"Space Sauce",60);
-        assertEquals("Space Sauce",ingredient.getName());
+    public void testIngredientGetPrice_NegativePriceAllowed() {
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Space Sauce", -60.02f);
+        assertEquals(-60.02f, ingredient.getPrice(), 0.0f);
     }
 
     @Test
-    public void testIngredientGetType(){
-        Ingredient ingredient = new Ingredient(IngredientType.FILLING,"Space Sauce",60);
-        assertEquals(IngredientType.FILLING,ingredient.getType());
+    public void testIngredientGetName() {
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Space Sauce", 60);
+        assertEquals("Space Sauce", ingredient.getName());
+    }
+
+    @Test
+    public void testIngredientGetType() {
+        Ingredient ingredient = new Ingredient(IngredientType.FILLING, "Space Sauce", 60);
+        assertEquals(IngredientType.FILLING, ingredient.getType());
     }
 }
