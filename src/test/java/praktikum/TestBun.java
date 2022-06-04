b3878e016bfd394eb391985adcbe0a1583402cd7
Marina Klimova <m.klim1005@gmail.com>
2022-06-04 21:38:56 +0300

    Refactor TestBun

--- src/test/java/praktikum/TestBun.java
+++ src/test/java/praktikum/TestBun.java
@@ -7,14 +7,20 @@ import static org.junit.Assert.assertEquals;
 public class TestBun {
 
     @Test
-    public void testBunGetName(){
-        Bun bun = new Bun("SomeBun",50);
-        assertEquals("SomeBun",bun.getName());
+    public void testBunGetName() {
+        Bun bun = new Bun("SomeBun", 50);
+        assertEquals("SomeBun", bun.getName());
     }
 
     @Test
-    public void testBunGetPrice(){
-        Bun bun = new Bun("SomeBun", 60);
-        assertEquals(60,bun.getPrice(),0.0f);
+    public void testBunGetPrice() {
+        Bun bun = new Bun("SomeBun", 60.11f);
+        assertEquals(60.11f, bun.getPrice(), 0.0f);
+    }
+
+    @Test
+    public void testBunGetPrice_NegativePriceAllowed() {
+        Bun bun = new Bun("SomeBun", -0.11f);
+        assertEquals(-0.11f, bun.getPrice(), 0.0f);
     }
 }
